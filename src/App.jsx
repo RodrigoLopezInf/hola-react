d7ffc1fce144647ff3ee56bfe78e0d848bf35526
Rodrigo Lopez <rodrigolopez686@gmail.com>
2026-05-25 20:46:51 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>hola mundo</h1>
+      <h2>iniciando en el mundo de la programación desde una mac</h2>
+      <p>este es un ejemplo en React que me esta llamndo la atenion jejeje</p>
+    </>
+  )
+}
+
+export default App
