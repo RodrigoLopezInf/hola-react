4d4ec7a361630d03b9c317f45a19adeb7eb1cebd
Rodrigo Lopez <rodrigolopez686@gmail.com>
2026-05-25 20:54:19 -0600

    EJEMPLO DE APP

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1>hola mundo</h1>
       <h2>iniciando en el mundo de la programación desde una mac</h2>
       <p>este es un ejemplo en React que me esta llamndo la atenion jejeje</p>
+      <H1>ESPERO QUE SIRVA</H1>
     </>
   )
 }
